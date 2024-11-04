b739b427451589ac0b5cbff4bb1ee85d735e2ce2
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-04 16:27:59 +0100

    refine: soluzione

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-const emailList = ["primo@gmail.com", "secondo@gmail.com", "terzo@gmail.com", "quarto@gmail.com"];
+const emailList = ["primo@gmail.com", "secondo@gmail.com", "terzo@gmail.com", "quarto@gmail.com", "quinto@gmail.com"];
 console.log(emailList);
 const emailUtente = prompt("dimmi la tua email");
-let verifica = "";
+let verifica = "non presente";
 for (let i = 0; i < emailList.length; i++){
     const emailpresent = emailList[i];
     if (emailpresent === emailUtente){
         verifica = "presente"
-        i = 100;
-    } else {
-        verifica = "non presente"
-    }
+        i = emailList.length +1;
+    } 
 }
 
 console.log("la tua email è " + verifica);
