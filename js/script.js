065d2d0914d233da1882c994f501f176ac368665
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-04 16:20:31 +0100

    feat: esercizio corretto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const emailList = ["primo@gmail.com", "secondo@gmail.com", "terzo@gmail.com", "quarto@gmail.com"];
 console.log(emailList);
 const emailUtente = prompt("dimmi la tua email");
-
+let verifica = "";
 for (let i = 0; i < emailList.length; i++){
     const emailpresent = emailList[i];
     if (emailpresent === emailUtente){
-        console.log("la tua email è presente nella lista benvenuto " + emailUtente);
+        verifica = "presente"
+        i = 100;
     } else {
-        console.log("la tua email non è presente");
+        verifica = "non presente"
     }
 }
+
+console.log("la tua email è " + verifica);
